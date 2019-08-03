69f8ce9d0566c543e07a43f7ac678d322a427b78
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-03 20:49:28 +0600

    internet check project intent added

--- InternetCheck/app/src/main/java/com/example/internetcheck/MainActivity.java
+++ InternetCheck/app/src/main/java/com/example/internetcheck/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.internetcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boolean activeNetwork = isNetworkActive();
+        if (activeNetwork){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -25,5 +33,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         unregisterReceiver(checkInternet);
+    }
+
+    public boolean isNetworkActive(){
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 }
