da38720395c61d96e086b11227b1db560dc76c01
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-03 20:12:52 +0600

    internet check projected updated

---
+++ InternetCheck/app/src/main/java/com/example/internetcheck/CheckInternet.java
@@ -0,0 +1,26 @@
+package com.example.internetcheck;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class CheckInternet extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        if(isConnected){
+            Toast.makeText(context, "internet Connected", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "internet disconnected", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- InternetCheck/app/src/main/java/com/example/internetcheck/MainActivity.java
+++ InternetCheck/app/src/main/java/com/example/internetcheck/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.internetcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CheckInternet checkInternet = new CheckInternet();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        registerReceiver(checkInternet, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(checkInternet);
     }
 }
