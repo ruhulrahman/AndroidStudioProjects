5da56068c3bba16b882c73624f222ab38d9b2fe1
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-05 19:11:11 +0600

    Service Project added

---
+++ Service/app/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,56 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private static final String TAG = MyService.class.getName();
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate: "+Thread.currentThread().getName());
+        new Async();
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    class Async extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
+}
