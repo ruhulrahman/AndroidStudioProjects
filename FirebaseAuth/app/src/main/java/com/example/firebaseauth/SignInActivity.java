5cec99b302532cca557ebebfb80a2d1c481081f8
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-05 18:03:11 +0600

    updated auth project

--- FirebaseAuth/app/src/main/java/com/example/firebaseauth/SignInActivity.java
+++ FirebaseAuth/app/src/main/java/com/example/firebaseauth/SignInActivity.java
@@ -1,14 +1,74 @@
 package com.example.firebaseauth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.firebaseauth.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private ActivitySignInBinding binding;
+    private String name, email, password;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+
+        init();
+        if(firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            finish();
+        }
+
+        binding.signUpTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = binding.emailET.getText().toString();
+                password = binding.passwordET.getText().toString();
+                signIn(email, password);
+            }
+        });
     }
+
+    private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+
+    private void signIn(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignInActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
--- FirebaseAuth/app/src/main/java/com/example/firebaseauth/SignUpActivity.java
+++ FirebaseAuth/app/src/main/java/com/example/firebaseauth/SignUpActivity.java
@@ -1,14 +1,72 @@
 package com.example.firebaseauth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.firebaseauth.databinding.ActivitySignInBinding;
+import com.example.firebaseauth.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private ActivitySignUpBinding binding;
+    private String name, email, password;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
+
+        init();
+
+
+        binding.singInTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                finish();
+            }
+        });
+
+        binding.singUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = binding.nameET.getText().toString();
+                email = binding.emailET.getText().toString();
+                password = binding.passwordET.getText().toString();
+                register(name, email, password);
+            }
+        });
+    }
+
+    private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void register(String name, String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
