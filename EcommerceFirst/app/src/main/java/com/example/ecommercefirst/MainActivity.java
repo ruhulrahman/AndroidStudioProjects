f8f33aaea100110e52d1114142f7a3eb8fb9b9df
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-08 02:22:25 +0600

    few changes

--- EcommerceFirst/app/src/main/java/com/example/ecommercefirst/MainActivity.java
+++ EcommerceFirst/app/src/main/java/com/example/ecommercefirst/MainActivity.java
@@ -7,12 +7,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.MenuItem;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,9 +39,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayoutId, fragment).commit();
     }
 
-    private ArrayList<Product> products;
-    private RecyclerView productRV;
-    private ProductAdapter productAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,41 +47,9 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         
-        init();
-        recyclerViewConfig();
-        getProducts();
+
     }
 
-    private void getProducts() {
-        Product product1 = new Product("Phone", "01", 2500, R.drawable.phone);
-        Product product2 = new Product("iPhone", "02", 7500, R.drawable.iphone);
-        Product product3 = new Product("Laptop", "03", 3500, R.drawable.laptop);
-        Product product4 = new Product("Laptop 1", "04", 50000, R.drawable.laptop1);
-        Product product5 = new Product("Camera", "05", 30000, R.drawable.camera);
-        Product product6 = new Product("Camera 1", "06", 32000, R.drawable.camera1);
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
-        products.add(product5);
-        products.add(product6);
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
-        products.add(product5);
-        products.add(product6);
-    }
 
-    private void recyclerViewConfig() {
-        productRV.setLayoutManager(new GridLayoutManager(this, 2));
-        productRV.setAdapter(productAdapter);
-    }
-
-    private void init() {
-        products = new ArrayList<>();
-        productRV = findViewById(R.id.itemRV);
-        productAdapter = new ProductAdapter(products);
-    }
 
 }
--- EcommerceFirst/app/src/main/java/com/example/ecommercefirst/ProductFragment.java
+++ EcommerceFirst/app/src/main/java/com/example/ecommercefirst/ProductFragment.java
@@ -1,23 +1,32 @@
 package com.example.ecommercefirst;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductFragment extends Fragment {
-
+    private ArrayList<Product> products;
+    private RecyclerView       productRV;
+    private ProductAdapter     productAdapter;
+    private Context context;
 
     public ProductFragment() {
         // Required empty public constructor
+
     }
 
 
@@ -25,7 +34,46 @@ public class ProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false);
+        View view = inflater.inflate(R.layout.fragment_product, container, false);
+        productRV = view.findViewById(R.id.itemRV);
+
+
+        init();
+        recyclerViewConfig();
+        getProducts();
+
+        return view;
     }
+    private void getProducts() {
+        Product product1 = new Product("Phone", "01", 2500, R.drawable.phone);
+        Product product2 = new Product("iPhone", "02", 7500, R.drawable.iphone);
+        Product product3 = new Product("Laptop", "03", 3500, R.drawable.laptop);
+        Product product4 = new Product("Laptop 1", "04", 50000, R.drawable.laptop1);
+        Product product5 = new Product("Camera", "05", 30000, R.drawable.camera);
+        Product product6 = new Product("Camera 1", "06", 32000, R.drawable.camera1);
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        products.add(product4);
+        products.add(product5);
+        products.add(product6);
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        products.add(product4);
+        products.add(product5);
+        products.add(product6);
+    }
+
+    private void recyclerViewConfig() {
+        productRV.setLayoutManager(new GridLayoutManager(context, 2));
+        productRV.setAdapter(productAdapter);
+    }
+
+    private void init() {
+        products = new ArrayList<>();
+        productAdapter = new ProductAdapter(products);
+    }
+
 
 }
