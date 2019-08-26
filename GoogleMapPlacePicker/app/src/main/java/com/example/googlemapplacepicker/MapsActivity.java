7266b3ff93aa6af73607542b9596762bc9548f2b
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-26 23:01:03 +0600

    GoogleMap Place and Picker Project Added

---
+++ GoogleMapPlacePicker/app/src/main/java/com/example/googlemapplacepicker/MapsActivity.java
@@ -0,0 +1,43 @@
+package com.example.googlemapplacepicker;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng bitm = new LatLng(23.7508671, 90.3913638);
+        mMap.addMarker(new MarkerOptions().position(bitm).title("BITM").snippet("BASIS Institute of Technology & Management (BITM)"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bitm, 15));
+    }
+
+    public void currectLocation(View view) {
+    }
+}
